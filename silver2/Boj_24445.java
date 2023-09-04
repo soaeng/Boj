e88baa23f93e3dda74fe93af1a8b0a1832d60bb7
soaeng <sodud0695@naver.com>
2023-09-04 19:44:20 +0900

    [ADD] 알고리즘 수업 - 너비 우선 탐색 2 (bfs)

---
+++ silver2/Boj_24445.java
@@ -0,0 +1,57 @@
+package silver2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/*
+    알고리즘 수업 - 너비 우선 탐색 2
+    https://www.acmicpc.net/problem/24445
+*/
+public class Boj_24445 {
+    static int N, M, R, count = 1;
+    static ArrayList<Integer>[] lists;
+    static int[] visited;
+
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        visited = new int[N + 1];
+        lists = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++)
+            lists[i] = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            lists[a].add(b);
+            lists[b].add(a);
+        }
+        for (int i = 1; i <= N; i++)
+            Collections.sort(lists[i], Collections.reverseOrder());
+        visited[R] = count++;
+        bfs();
+        StringBuilder sb = new StringBuilder();
+        for (int v : visited) sb.append(v).append("\n");
+        sb.delete(0, 2);
+        System.out.println(sb);
+    }
+
+    private static void bfs() {
+        Queue<Integer> queue = new LinkedList<>(lists[R]);
+        while (!queue.isEmpty()) {
+            int n = queue.poll();
+            if (visited[n] == 0) {
+                visited[n] = count++;
+                queue.addAll(lists[n]);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Boj_24445().solution();
+    }
+}
