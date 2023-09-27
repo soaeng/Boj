1c7ffca04e02cb45b65416485888f0fc7cf25ec9
soaeng <sodud0695@naver.com>
2023-09-27 20:09:22 +0900

    [ADD] 미네랄 (bfs, 시뮬레이션)

---
+++ gold1/Boj_18500.java
@@ -0,0 +1,15 @@
+package gold1;
+
+/*
+    미네랄 2
+    https://www.acmicpc.net/problem/18500
+*/
+public class Boj_18500 {
+    public void solution() throws Exception {
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Boj_18500().solution();
+    }
+}
