d39737963ec83d851920d708da686fc7fbf5dbc2
soaeng <sodud0695@naver.com>
2023-12-19 12:03:44 +0900

    [ADD] 색종이 (구현)

---
+++ silver5/Boj_2563.java
@@ -0,0 +1,38 @@
+package silver5;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+    색종이
+    https://www.acmicpc.net/problem/2563
+*/
+public class Boj_2563 {
+    public void solution() throws Exception {
+        int[][] map = new int[101][101];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for (int n = 0; n < N; n++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            for (int i = x; i < x + 10; i++) {
+                for (int j = y; j < y + 10; j++) {
+                    map[i][j] = 1;
+                }
+            }
+        }
+        int area = 0;
+        for (int[] bb : map) {
+            for (int b : bb) {
+                area += b;
+            }
+        }
+        System.out.println(area);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Boj_2563().solution();
+    }
+}
