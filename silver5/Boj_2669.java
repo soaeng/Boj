30cf7fd490775ec72d7721f5294b5c610baa19c5
soaeng <sodud0695@naver.com>
2023-12-23 03:20:11 +0900

    [UPDATE] 직사각형 네개의 합집합의 면적 구하기 (구현)

--- silver5/Boj_2669.java
+++ silver5/Boj_2669.java
@@ -2,7 +2,6 @@ package silver5;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 /*
@@ -12,31 +11,23 @@ import java.util.StringTokenizer;
 public class Boj_2669 {
     public void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int maxR = 0;
-        int maxC = 0;
-        ArrayList<int[]> spots = new ArrayList<>();
+        boolean[][] map = new boolean[100][100];
+        int ans = 0;
         for (int i = 0; i < 4; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int c1 = Integer.parseInt(st.nextToken());
             int r1 = Integer.parseInt(st.nextToken());
             int c2 = Integer.parseInt(st.nextToken());
             int r2 = Integer.parseInt(st.nextToken());
-            maxR = Math.max(maxR, r2);
-            maxC = Math.max(maxC, c2);
-            spots.add(new int[]{r1, c1, r2, c2});
-        }
-        int[][] square = new int[maxR][maxC];
-        for (int[] spot : spots) {
-            for (int r = spot[0]; r < spot[2]; r++) {
-                for (int c = spot[1]; c < spot[3]; c++) {
-                    square[r][c] = 1;
+            for (int r = r1; r < r2; r++) {
+                for (int c = c1; c < c2; c++) {
+                    map[r][c] = true;
                 }
             }
         }
-        int ans = 0;
-        for (int[] nn : square) {
-            for (int n : nn) {
-                if (n == 1) ans++;
+        for (boolean[] mm : map) {
+            for (boolean m : mm) {
+                if (m) ans++;
             }
         }
         System.out.println(ans);
