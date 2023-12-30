231147a23a139c7c511aa3e4946fd44a20ab7966
soaeng <sodud0695@naver.com>
2023-12-30 20:42:06 +0900

    [UPDATE] 방 번호 (구현)

--- silver5/Boj_1475.java
+++ silver5/Boj_1475.java
@@ -11,17 +11,17 @@ public class Boj_1475 {
     public void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int set = 0;
-        String N = br.readLine();
-        int[] numbers = new int[10];
-        for (int i = 0; i < N.length(); i++) {
-            if (N.charAt(i) - '0' == 6) numbers[9]++;
-            else numbers[N.charAt(i) - '0']++;
+        int[] numbers = new int[9];
+        for (char ch : br.readLine().toCharArray()) {
+            int n = ch - '0';
+            if (n == 9) numbers[6]++;
+            else numbers[n]++;
         }
+        numbers[6] = numbers[6] % 2 == 0 ? (numbers[6] / 2) : ((numbers[6] + 1) / 2);
         for (int i = 0; i < 9; i++) {
             set = Math.max(set, numbers[i]);
         }
-        if (numbers[9] % 2 != 0) numbers[9]++;
-        System.out.println(Math.max(set, numbers[9] / 2));
+        System.out.println(set);
     }
 
     public static void main(String[] args) throws Exception {
