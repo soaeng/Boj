7eaaa29b829eed4429f3042ef0d16fb91790cc5f
soaeng <sodud0695@naver.com>
2024-01-25 15:13:39 +0900

    [UPDATE]

--- Main.java
+++
@@ -1,9 +0,0 @@
-import bronze5.Boj_2438;
-
-// https://covenant.tistory.com/224
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        new Boj_2438().solution();
-    }
-}
