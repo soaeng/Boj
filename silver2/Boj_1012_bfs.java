5675ae98a1954755f41110731361c96a09953ee5
soaeng <sodud0695@naver.com>
2024-01-26 22:21:31 +0900

    [UPDATE] 유기농 배추 (bfs)

--- silver2/Boj_1012_bfs.java
+++ silver2/Boj_1012_bfs.java
@@ -1,2 +1,80 @@
-package silver2;public class Boj_1012_bfs {
+package silver2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/*
+    유기농 배추
+    https://www.acmicpc.net/problem/1012
+*/
+public class Boj_1012_bfs {
+    static int M, N, K;
+    static boolean[][] map;
+    static boolean[][] visited;
+
+    private static void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 0; tc < T; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int count = 0;
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+            map = new boolean[N][M];
+            visited = new boolean[N][M];
+            for (int i = 0; i < K; i++) {
+                st = new StringTokenizer(br.readLine());
+                int c = Integer.parseInt(st.nextToken());
+                int r = Integer.parseInt(st.nextToken());
+                map[r][c] = true;
+            }
+            for (int r = 0; r < N; r++) {
+                for (int c = 0; c < M; c++) {
+                    if (visited[r][c]) continue;
+                    if (!map[r][c]) continue;
+                    count++;
+                    bfs(r, c);
+                }
+            }
+            sb.append(count).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void bfs(int r, int c) {
+        int[][] deltas = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.offer(new Node(r, c));
+        visited[r][c] = true;
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            for (int d = 0; d < 4; d++) {
+                int nr = node.r + deltas[d][0];
+                int nc = node.c + deltas[d][1];
+                if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
+                if (visited[nr][nc]) continue;
+                if (!map[nr][nc]) continue;
+                queue.offer(new Node(nr, nc));
+                visited[nr][nc] = true;
+            }
+        }
+    }
+
+    private static class Node {
+        int r, c;
+
+        public Node(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        solution();
+    }
 }
--- silver2/Boj_1012_dfs.java
+++ silver2/Boj_1012_dfs.java
@@ -2,77 +2,58 @@ package silver2;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 /*
     유기농 배추
     https://www.acmicpc.net/problem/1012
 */
-public class Boj_1012 {
-    static int T, M, N, K, ans = 0;
-    static int[][] map;
+public class Boj_1012_dfs {
+    static int M, N, K;
+    static boolean[][] map;
     static int[][] deltas = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    static Queue<Node> queue;
 
-    public void solution() throws Exception {
+    private static void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
         for (int t = 0; t < T; t++) {
+            int count = 0;
             StringTokenizer st = new StringTokenizer(br.readLine());
             M = Integer.parseInt(st.nextToken());
             N = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
-            map = new int[N][M];
-            ans = 0;
+            map = new boolean[N][M];
             for (int k = 0; k < K; k++) {
                 st = new StringTokenizer(br.readLine());
-                int m = Integer.parseInt(st.nextToken());
-                int n = Integer.parseInt(st.nextToken());
-                map[n][m] = 1;
+                int c = Integer.parseInt(st.nextToken());
+                int r = Integer.parseInt(st.nextToken());
+                map[r][c] = true;
             }
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    if (map[i][j] == 1) {
-                        bfs(i, j);
-                        ans++;
-                    }
+            for (int r = 0; r < N; r++) {
+                for (int c = 0; c < M; c++) {
+                    if (!map[r][c]) continue;
+                    count++;
+                    dfs(r, c);
                 }
             }
-            sb.append(ans).append("\n");
+            sb.append(count).append("\n");
         }
         System.out.println(sb);
     }
 
-    static void bfs(int i, int j) {
-        queue = new LinkedList<>();
-        queue.offer(new Node(i, j));
-        Node node;
-        while (!queue.isEmpty()) {
-            node = queue.poll();
-            for (int d = 0; d < 4; d++) {
-                int nr = node.r + deltas[d][0];
-                int nc = node.c + deltas[d][1];
-                if (nr >= 0 && nr < N && nc >= 0 && nc < M && map[nr][nc] == 1) {
-                    queue.offer(new Node(nr, nc));
-                    map[nr][nc] = 0;
-                }
-            }
-        }
-    }
-
-    static class Node {
-        int r, c;
-
-        public Node(int r, int c) {
-            this.r = r;
-            this.c = c;
+    static void dfs(int r, int c) {
+        map[r][c] = false;
+        for (int d = 0; d < 4; d++) {
+            int nr = r + deltas[d][0];
+            int nc = c + deltas[d][1];
+            if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
+            if (!map[nr][nc]) continue;
+            dfs(nr, nc);
         }
     }
 
     public static void main(String[] args) throws Exception {
-        new Boj_1012().solution();
+        solution();
     }
 }
