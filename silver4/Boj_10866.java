8c2befcf4af2d72ec0b4194e3f91926456675624
soaeng <sodud0695@naver.com>
2024-02-14 17:44:03 +0900

    [ADD] 덱

---
+++ silver4/Boj_10866.java
@@ -0,0 +1,54 @@
+package silver4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+/*
+    덱
+    https://www.acmicpc.net/problem/10866
+*/
+public class Boj_10866 {
+    private static void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Deque<Integer> deque = new ArrayDeque<>();
+        int N = Integer.parseInt(br.readLine());
+        while (N-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            switch (st.nextToken()) {
+                case "push_front":
+                    deque.offerFirst(Integer.parseInt(st.nextToken()));
+                    break;
+                case "push_back":
+                    deque.offerLast(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop_front":
+                    sb.append(deque.isEmpty() ? -1 : deque.pollFirst()).append("\n");
+                    break;
+                case "pop_back":
+                    sb.append(deque.isEmpty() ? -1 : deque.pollLast()).append("\n");
+                    break;
+                case "size":
+                    sb.append(deque.size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(deque.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                case "front":
+                    sb.append(deque.isEmpty() ? -1 : deque.peekFirst()).append("\n");
+                    break;
+                case "back":
+                    sb.append(deque.isEmpty() ? -1 : deque.peekLast()).append("\n");
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) throws Exception {
+        solution();
+    }
+}
