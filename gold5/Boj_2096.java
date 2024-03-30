2e91c66850c3fde189533b2f95457b7010b72587
soaeng <sodud0695@naver.com>
2024-03-30 16:10:06 +0900

    [UPDATE] 내려가기 (dp)

--- gold5/Boj_2096.java
+++ gold5/Boj_2096.java
@@ -13,32 +13,30 @@ public class Boj_2096 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[][] arr = new int[N][3];
-        int[][] dp = new int[N][3];
+        int[] dp = new int[3];
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 3; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        System.arraycopy(arr[0], 0, dp[0], 0, 3);
+        System.arraycopy(arr[0], 0, dp, 0, 3);
         for (int i = 1; i < N; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]) + arr[i][0];
-            dp[i][1] = Math.max(dp[i - 1][0], Math.max(dp[i - 1][1], dp[i - 1][2])) + arr[i][1];
-            dp[i][2] = Math.max(dp[i - 1][1], dp[i - 1][2]) + arr[i][2];
+            int[] temp = {dp[0], dp[1], dp[2]};
+            dp[0] = Math.max(temp[0], temp[1]) + arr[i][0];
+            dp[1] = Math.max(temp[0], Math.max(temp[1], temp[2])) + arr[i][1];
+            dp[2] = Math.max(temp[1], temp[2]) + arr[i][2];
         }
         StringBuilder sb = new StringBuilder();
-        N--;
-        sb.append(Math.max(dp[N][0], Math.max(dp[N][1], dp[N][2]))).append(" ");
-        N++;
-        dp = new int[N][3];
-        System.arraycopy(arr[0], 0, dp[0], 0, 3);
+        sb.append(Math.max(dp[0], Math.max(dp[1], dp[2]))).append(" ");
+        System.arraycopy(arr[0], 0, dp, 0, 3);
         for (int i = 1; i < N; i++) {
-            dp[i][0] = Math.min(dp[i - 1][0], dp[i - 1][1]) + arr[i][0];
-            dp[i][1] = Math.min(dp[i - 1][0], Math.min(dp[i - 1][1], dp[i - 1][2])) + arr[i][1];
-            dp[i][2] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][2];
+            int[] temp = {dp[0], dp[1], dp[2]};
+            dp[0] = Math.min(temp[0], temp[1]) + arr[i][0];
+            dp[1] = Math.min(temp[0], Math.min(temp[1], temp[2])) + arr[i][1];
+            dp[2] = Math.min(temp[1], temp[2]) + arr[i][2];
         }
-        N--;
-        sb.append(Math.min(dp[N][0], Math.min(dp[N][1], dp[N][2])));
+        sb.append(Math.min(dp[0], Math.min(dp[1], dp[2]))).append(" ");
         System.out.println(sb);
     }
 
