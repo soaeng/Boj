80fbc20b75002cea4da9e3a04876d08ea8dc8f89
soaeng <sodud0695@naver.com>
2024-03-30 15:08:01 +0900

    [UPDATE] 폰켓몬 (자료구조)

--- programmers/Pro_1845.java
+++ programmers/Pro_1845.java
@@ -1,8 +1,6 @@
 package programmers;
 
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -14,15 +12,6 @@ public class Pro_1845 {
 
     public int solution(int[] nums) {
         Set<Integer> set = new HashSet<>();
-        int[] arr = {4, 4, 4, 3, 3};
-        ArrayDeque<Integer> stack = new ArrayDeque<>();
-
-        for (int n : arr) {
-            if (!stack.isEmpty() && stack.peekLast() == n) continue;
-            stack.offerLast(n);
-        }
-        int[] answer = stack.stream().mapToInt(Integer::intValue).toArray();
-        System.out.println(Arrays.toString(answer));
         for (int num : nums) set.add(num);
         return Math.min(nums.length / 2, set.size());
     }
