c9b95d8c5ed6a2a276f8f11fca0f556ab4cad134
soaeng <sodud0695@naver.com>
2024-04-24 22:22:25 +0900

    [ADD] 전깃줄 (dp, LIS)

---
+++ gold5/Boj_2565.java
@@ -0,0 +1,44 @@
+package gold5;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+/*
+    전깃줄
+    https://www.acmicpc.net/problem/2565
+*/
+public class Boj_2565 {
+    private static void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+        int[] dp = new int[N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr, new Comparator<>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i][1] > arr[j][1]) dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(N - max);
+    }
+
+    public static void main(String[] args) throws Exception {
+        solution();
+    }
+}
