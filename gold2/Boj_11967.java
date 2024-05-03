fbfc6777b81234630073f93f7a75b7e5a203c3c9
soaeng <sodud0695@naver.com>
2024-05-03 20:40:42 +0900

    [UPDATE] 불켜기 (bfs)

--- gold2/Boj_11967.java
+++ gold2/Boj_11967.java
@@ -10,20 +10,26 @@ import java.util.*;
 */
 public class Boj_11967 {
     static int N;
-    static Map<Integer, ArrayList<Integer>> map;
+    static ArrayList<Room>[][] switches;
 
     private static void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        map = new HashMap<>();
+        switches = new ArrayList[N][N];
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                switches[r][c] = new ArrayList<>();
+            }
+        }
         while (M-- > 0) {
             st = new StringTokenizer(br.readLine());
-            int key = (Integer.parseInt(st.nextToken()) - 1) * N + (Integer.parseInt(st.nextToken()) - 1);
-            ArrayList<Integer> list = map.getOrDefault(key, new ArrayList<>());
-            list.add((Integer.parseInt(st.nextToken()) - 1) * N + (Integer.parseInt(st.nextToken()) - 1));
-            map.put(key, list);
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+            switches[x][y].add(new Room(a, b));
         }
         System.out.println(bfs());
     }
@@ -31,33 +37,37 @@ public class Boj_11967 {
     private static int bfs() {
         int[][] deltas = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         Queue<Room> queue = new ArrayDeque<>();
-        boolean[][][] visited = new boolean[2][N][N];
+        boolean[][] visited = new boolean[N][N];
+        boolean[][] light = new boolean[N][N];
         queue.offer(new Room(0, 0));
-        visited[1][0][0] = true;
+        visited[0][0] = true;
+        light[0][0] = true;
         int count = 1;
         while (!queue.isEmpty()) {
             Room room = queue.poll();
-            // 불 다 켜기
-            if (map.get(room.id) != null) {
-                visited[0] = new boolean[N][N];
-                visited[0][room.r][room.c] = true;
-                for (int id : map.get(room.id)) {
-                    int r = id / N;
-                    int c = id % N;
-                    if (visited[1][r][c]) continue;
-                    visited[1][r][c] = true;
-                    count++;
-                }
-                map.remove(room.id);
-            }
             for (int d = 0; d < 4; d++) {
                 int nr = room.r + deltas[d][0];
                 int nc = room.c + deltas[d][1];
                 if (isOutOfRange(nr, nc)) continue;
-                if (visited[0][nr][nc]) continue;
-                if (!visited[1][nr][nc]) continue;
+                if (visited[nr][nc]) continue;
+                if (!light[nr][nc]) continue;
                 queue.offer(new Room(nr, nc));
-                visited[0][nr][nc] = true;
+                visited[nr][nc] = true;
+            }
+            for (Room r : switches[room.r][room.c]) {
+                if (!light[r.r][r.c]) count++;
+                light[r.r][r.c] = true;
+                if (visited[r.r][r.c]) continue;
+                for (int d = 0; d < 4; d++) {
+                    int nr = r.r + deltas[d][0];
+                    int nc = r.c + deltas[d][1];
+                    if (isOutOfRange(nr, nc)) continue;
+                    if (visited[nr][nc]) {
+                        visited[r.r][r.c] = true;
+                        queue.offer(new Room(r.r, r.c));
+                        break;
+                    }
+                }
             }
         }
         return count;
@@ -68,17 +78,11 @@ public class Boj_11967 {
     }
 
     private static class Room {
-        int id, r, c;
+        int r, c;
 
         public Room(int r, int c) {
-            this.id = r * N + c;
             this.r = r;
             this.c = c;
-        }
-
-        @Override
-        public String toString() {
-            return "(" + id + ", " + r + ", " + c + ")";
         }
     }
 
